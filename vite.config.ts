8f0c4cc1352e83feaa771123726bc2665122497b
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2025-03-30 01:16:08 +0000

    inital 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Brain3/', // Make sure this matches your repository name
+    base: '/', // Make sure this matches your repository name
     plugins: [react()],
 });
